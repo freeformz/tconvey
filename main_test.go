c77927ce22f2158ebeed92b19ad4060f6d232179
Edward Muller <edwardam@interlix.com>
2016-05-19 15:43:06 -0700

    What

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSpec(t *testing.T) {
+	Convey("Given a thing", t, func() {
+		x := 1
+
+		Convey("When the integer is incremented", func() {
+			x++
+
+			Convey("The value should be greater by one", func() {
+				So(x, ShouldEqual, 2)
+			})
+		})
+	})
+}
